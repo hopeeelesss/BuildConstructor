74969b089a7402c8c5f8dc50d2444d2de2b4b966
Victor <khokhlov34@gmail.com>
2023-05-23 18:42:39 +0300

    OAuth rollback

--- src/main/java/com/example/buildconstructor/controllers/UserController.java
+++ src/main/java/com/example/buildconstructor/controllers/UserController.java
@@ -2,8 +2,8 @@ package com.example.buildconstructor.controllers;
 
 import com.example.buildconstructor.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
+//import org.springframework.security.core.annotation.AuthenticationPrincipal;
+//import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,9 +14,9 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @GetMapping("main")
-    private String register(@AuthenticationPrincipal OAuth2User user){
-        userService.register(user);
-        return "Welcome!";
-    }
+//    @GetMapping("main")
+//    private String register(@AuthenticationPrincipal OAuth2User user){
+//        userService.register(user);
+//        return "Welcome!";
+//    }
 }
--- src/main/java/com/example/buildconstructor/services/UserService.java
+++ src/main/java/com/example/buildconstructor/services/UserService.java
@@ -4,7 +4,7 @@ import com.example.buildconstructor.entities.Role;
 import com.example.buildconstructor.entities.User;
 import com.example.buildconstructor.repositories.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.oauth2.core.user.OAuth2User;
+//import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,18 +12,18 @@ public class UserService {
     @Autowired
     private UserRepo userRepo;
 
-    public void register(OAuth2User user){
-        User checkUser= new User();
-        if(userRepo.findUserByLogin(
-                (String) user.getAttributes().
-                        get("login"))==null){
-            checkUser.setLogin((String) user.getAttributes().
-                    get("login"));
-            checkUser.setServiceId((Integer) user.getAttributes().
-                    get("id"));
-            checkUser.setUserId(Long.valueOf(user.getName()));
-            checkUser.setRole(Role.USER);
-            userRepo.save(checkUser);
-        }
-    }
+//    public void register(OAuth2User user){
+//        User checkUser= new User();
+//        if(userRepo.findUserByLogin(
+//                (String) user.getAttributes().
+//                        get("login"))==null){
+//            checkUser.setLogin((String) user.getAttributes().
+//                    get("login"));
+//            checkUser.setServiceId((Integer) user.getAttributes().
+//                    get("id"));
+//            checkUser.setUserId(Long.valueOf(user.getName()));
+//            checkUser.setRole(Role.USER);
+//            userRepo.save(checkUser);
+//        }
+//    }
 }
